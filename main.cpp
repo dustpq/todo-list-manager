22bc025efb31120ce645950412296c9995d13e1b
dust.pq <portillo0121@gmail.com>
2025-09-05 11:11:31 -0400

    added task declaration and initialization for testing

--- main.cpp
+++ main.cpp
@@ -9,10 +9,19 @@ int main() {
   string input1, input2, input3;
 
   std::cout << "Enter task name: ";
-  std::cin >> input1;
+  std::getline(std::cin, input1);
 
   std::cout << "Enter task date: ";
-  std::cin >> input2;
+  std::getline(std::cin, input2);
+
+  std::cout << "Enter task time: ";
+  std::getline(std::cin, input3);
+
+  Task task1(input1, input2, input3);
+
+  std::cout << "You have made a task with the following attributes:\n";
+  std::cout << "Name: " + task1.getName() + "\nDate: " + task1.getDate() +
+                   "\nTime: " + task1.getTime();
 
   return 0;
 }
