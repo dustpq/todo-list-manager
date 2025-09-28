e14c0ab28afb73f4c3caade7159824967aef4469
dust.pq <portillo0121@gmail.com>
2025-09-28 17:45:24 -0400

    using new Console statements for testing

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include "Console.h"
 #include "Task.h"
-#include <iostream>
 #include <string>
 
 using std::string;
@@ -12,22 +11,19 @@ int main() {
   string input1, input2, input3;
 
   Console::outputMessage("Enter task name: ");
-  std::getline(std::cin, input1);
   input1 = Console::getInput();
 
   Console::outputMessage("Enter task date: ");
-  std::getline(std::cin, input1);
   input2 = Console::getInput();
 
   Console::outputMessage("Enter task time: ");
-  std::getline(std::cin, input1);
   input3 = Console::getInput();
 
   Task task(input1, input2, input3);
 
-  Console::outputMessage("Created task:\n" + "Task name: " + task.getName() +
-                         "\nTask Date: " + task.getDate() +
-                         "\nTask Time: " + task.getTime());
+  Console::outputMessage("Created task:\nTask name: ", task.getName(),
+                         "\nTask Date: ", task.getDate(),
+                         "\nTask Time: ", task.getTime(), "\n");
 
   Console::pauseForInput();
 
